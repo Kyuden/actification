3190d489c350c4b280935e0d7bd6a8bba9082c01
Kyuden <msmsms.um@gmail.com>
2015-02-17 02:19:35 +0900

    Use source_root

--- lib/generators/actification/migration/migration_generator.rb
+++ lib/generators/actification/migration/migration_generator.rb
@@ -1,10 +1,9 @@
-require 'generators/actification'
 require 'rails/generators/active_record'
 
 module Actification
   module Generators
     class MigrationGenerator < ActiveRecord::Generators::Base
-      extend Base
+      source_root File.expand_path("../templates", __FILE__)
 
       argument :name, type: :string, default: 'create_actifications'
 
