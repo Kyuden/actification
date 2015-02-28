12d46927197baa80aa857534336535ccf8d684e8
Kyuden <msmsms.um@gmail.com>
2015-02-28 20:05:32 +0900

    Can use dafault values

--- lib/actification/base.rb
+++ lib/actification/base.rb
@@ -1,5 +1,10 @@
+require 'active_support/core_ext/class'
+
 module Actification
   class Base
+    class_attribute :default_params
+    self.default_params = {}
+
     class << self
       def method_missing(method_name, *args)
         if public_instance_methods(false).include?(method_name)
@@ -8,10 +13,15 @@ module Actification
           super
         end
       end
+
+      def default(value=nil)
+        self.default_params = default_params.merge(value) if value
+        default_params
+      end
     end
 
     def actice(attr={})
-      Model.create_actification(attr)
+      Model.create_actification(default_params.merge(attr))
     end
   end
 end
