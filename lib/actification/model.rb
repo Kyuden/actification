c33d209cf844a476b9c68bf660e800705c9a9fda
Kyuden <msmsms.um@gmail.com>
2015-03-01 03:28:04 +0900

    Add scope about read/unread

--- lib/actification/model.rb
+++ lib/actification/model.rb
@@ -2,6 +2,14 @@ module Actification
   class Model < ActiveRecord::Base
     self.table_name = "actifications"
 
+    scope :read,      -> { where(read: true) }
+    scope :unread,    -> { where(read: false) }
+    scope :to,        -> (usre_id) { where(to: user_id)}
+    scope :from,      -> (usre_id) { where(from: user_id)}
+    scope :unread_by, -> (user_id) { unread.to(user_id) }
+    scope :read_by,   -> (user_id) { read.to(user_id) }
+    scope :newly,     -> { order(id: :desc) }
+
     class << self
       def create_actification(attr)
         Array(attr[:to]).each do |user_id|
